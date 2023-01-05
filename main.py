5550abffdd17a6ad9807d4f9aed4064b1a660147
piotr.gliszczynski@gmail.com <piotr.gliszczynski@gmail.com>
2023-01-05 18:45:46 +0100

    Few examples of openCV functionalities

--- main.py
+++ main.py
@@ -1,43 +1,105 @@
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
+import numpy as np
 from ponggame import ponggame
 
+def nothing(x):
+    pass
 
 if __name__ == "__main__":
     cap = cv.VideoCapture(0)
     first = True
+
+    cv.namedWindow('image')
+    cv.createTrackbar('Value 1', 'image', 0, 255, nothing)
+    cv.createTrackbar('Value 2', 'image', 0, 255, nothing)
+
+
+
     while(1):
-        # Take each frame
+
         _, frame = cap.read()
-        if first:
-            new_game = ponggame(frame.shape[0],frame.shape[1])
-            first = False
-        # Convert BGR to HSV
-        new_frame = frame.copy()
-        new_frame2 = frame.copy()
-        cv.rectangle(new_frame2, (0, 0), (640, 480), (255, 0, 255), -1)
-        cv.rectangle(new_frame, (20, 0), (620, 480), (255, 255, 255), -1)
-        hsv = cv.cvtColor(new_frame, cv.COLOR_BGR2HSV)
-        # define range of blue color in HSV
-        lower = np.array([0,0,0])
-        upper = np.array([255,255,20])
-        # Threshold the HSV image to get only blue colors
-        mask = cv.inRange(hsv, lower, upper)
-        cv.imshow('mask', mask)
-        mask = cv.bitwise_not(mask)
-        # Bitwise-AND mask and original image
 
-        res = cv.bitwise_or(new_frame2, frame, mask = mask)
+        value1 = cv.getTrackbarPos('Value 1', 'image')
+        value2 = cv.getTrackbarPos('Value 2', 'image')
 
-        new_game.nastpena_klatka()
-        new_game.rysuj_pilke(res)
+        src_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        #cv.imshow('frame',frame)
+        after_cv = frame
+
+        ### Threshold https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
+        _, after_cv = cv.threshold(src_gray, value1, value2, cv.THRESH_TOZERO)
+        # _, after_cv = cv.threshold(src_gray, value1, value2, cv.THRESH_BINARY_INV)
+
+        ### image smooting - avreging https://docs.opencv.org/4.x/d4/d13/tutorial_py_filtering.html
+        # if first:
+        #     cv.setTrackbarPos('Value 1', 'image', 110)
+        # kernel_size = int((value1 // 25.5)*2+1)
+        # kernel = np.ones((kernel_size, kernel_size), np.float32) / (kernel_size**2)
+        # dst = cv.filter2D(frame, -1, kernel)
+        # after_cv = dst
+
+        ### Color thresholding https://docs.opencv.org/3.4/da/d97/tutorial_threshold_inRange.html
+        # if first:
+        #     cv.setTrackbarPos('Value 1', 'image', 110)
+        #     cv.setTrackbarPos('Value 2', 'image', 255)
+        #
+        # frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        # kernel = np.ones((5, 5), np.float32) / 25
+        # dst = cv.filter2D(frame_HSV, -1, kernel)
+        # mask = cv.inRange(dst, (value1, 20, value2), (value1+50, 255, value2+100))
+        # after_cv = cv.bitwise_and(frame,frame,mask=mask)
+
+        ### Canny edge detection https://docs.opencv.org/3.4/da/d22/tutorial_py_canny.html
+        # after_cv = cv.Canny(src_gray, value1, value2)
+
+        ### Template matching https://docs.opencv.org/3.4/d4/dc6/tutorial_py_template_matching.html
+        # if first:
+        #     template = cv.imread('photo.jpeg', 0)
+        #     w, h = template.shape[::-1]
+        # res = cv.matchTemplate(src_gray, template, cv.TM_CCOEFF)
+        # min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+        # top_left = max_loc
+        # bottom_right = (top_left[0] + w, top_left[1] + h)
+        # after_cv = frame
+        # cv.rectangle(after_cv, top_left, bottom_right, (255,0,0), 2)
+
+        ### Polygon detection https://www.geeksforgeeks.org/python-detect-polygons-in-an-image-using-opencv/
+        # if first:
+        #     cv.setTrackbarPos('Value 1', 'image', 110)
+        #     cv.setTrackbarPos('Value 2', 'image', 255)
+        # _, threshold = cv.threshold(src_gray, value1, value2,
+        #                              cv.THRESH_BINARY)
+        # contours, _ = cv.findContours(threshold, cv.RETR_TREE,
+        #                                cv.CHAIN_APPROX_SIMPLE)
+        # for cnt in contours:
+        #     approx = cv.approxPolyDP(cnt,
+        #                               0.009 * cv.arcLength(cnt, True), True)
+        #     area = cv.contourArea(cnt)
+        #     if ( area > 200 ) and (len(approx) < 30):
+        #         cv.drawContours(after_cv, [approx], 0, (0, 0, 255), 2)
 
 
-        cv.imshow('res',res)
+        cv.imshow('image', after_cv)
+
+        ### pong
+        #
+        # if first:
+        #     new_game = ponggame(frame.shape[0],frame.shape[1])
+        #
+        # kernel = np.ones((5, 5), np.float32) / 25
+        # scr_gray = cv.filter2D(src_gray, -1, kernel)
+        # _, thresh4 = cv.threshold(src_gray, value1, 255, cv.THRESH_BINARY_INV)
+        #
+        # for _ in range(5):
+        #     new_game.nastpena_klatka(thresh4)
+        # new_game.rysuj_pilke(thresh4)
+        # cv.imshow('2', thresh4)
+
         k = cv.waitKey(5) & 0xFF
         if k == 27:
             break
+        first = False
 
     cv.destroyAllWindows()
--- ponggame.py
+++ ponggame.py
@@ -1,11 +1,12 @@
 import cv2 as cv
 
 class ball:
-    def __init__(self, start_x, start_y):
-        self.wektor_x = 10.0
-        self.wektor_y = 10.0
+    def __init__(self, start_x, start_y, ball_jump):
+        self.wektor_x = ball_jump
+        self.wektor_y = ball_jump
         self.pozycja_x = start_x
         self.pozycja_y = start_y
+        self.ball_jump = ball_jump
 
     def nowa_pozycja(self):
        # print(self.pozycja_x,self.pozycja_y)
@@ -15,6 +16,12 @@ class ball:
     def odbij_x(self):
         self.wektor_x *= -1
 
+    def odbij_w_prawo(self):
+        self.wektor_x = self.ball_jump
+
+    def odbij_w_lewo(self):
+        self.wektor_x = -self.ball_jump
+
     def odbij_y(self):
         self.wektor_y *= -1
 
@@ -22,16 +29,25 @@ class ball:
 class ponggame:
     def __init__(self,height,width):
        # print(f"Stworzono nową grę {width}, {height}")
+        self.ball_jump = 1
         self.width = width
         self.height = height
-        self.ball = ball(width // 2, height // 2 )
+        self.ball = ball(self.width // 2, self.height // 2 , self.ball_jump)
 
-    def nastpena_klatka(self):
+    def nastpena_klatka(self,image):
+        # sprawdź czy na drodze jest przeszkoda
+        crop = image[self.ball.pozycja_y-1:self.ball.pozycja_y+1,self.ball.pozycja_x-1:self.ball.pozycja_x+1]
+        if crop.mean() > 0:
+            if self.ball.pozycja_x < (self.width/2):
+                self.ball.odbij_w_prawo()
+            else:
+                self.ball.odbij_w_lewo()
         # sprawdź czy pozycja jest prz brzegu
         if (self.ball.pozycja_x < 10) or (self.ball.pozycja_x > (self.width - 10)):
             self.ball.odbij_x()
         if (self.ball.pozycja_y < 10) or (self.ball.pozycja_y > (self.height - 10)):
             self.ball.odbij_y()
+
         self.ball.nowa_pozycja()
 
     def rysuj_pilke(self,image):
